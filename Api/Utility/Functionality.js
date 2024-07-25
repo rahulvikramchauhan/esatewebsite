1ff43875e4d97568994d77556dcb11618bb53588
rahul chauhan <128809721+rahulvikramchauhan@users.noreply.github.com>
2024-07-25 17:47:01 +0530

    Update Functionality.js

--- Api/Utility/Functionality.js
+++ Api/Utility/Functionality.js
@@ -1,12 +1,13 @@
-const bcrypt = require("bcrypt");
+const argon = require("argon2");
 exports.createPassword = async function (password) {
     const saltRound = 10;//use to make hasing more secure 
     //and computational
-    const hasedPassword = await bcrypt.hash(password, saltRound);
+    const hasedPassword = await argon.hash(password);
+    console.log(hasedPassword)
     return hasedPassword;
 }
 exports.checkPassword = async function (password, hasedPassword) {
+    console.log(await argon.verify(password, hasedPassword));
 
-
-    return await bcrypt.compare(password, hasedPassword);
+    return await argon.verify(password, hasedPassword);
 }
