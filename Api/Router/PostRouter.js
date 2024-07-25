8d50c1647f46adfa1e0d8639263b120bfefa17a4
rahul chauhan <128809721+rahulvikramchauhan@users.noreply.github.com>
2024-07-25 17:50:25 +0530

    Update PostRouter.js

--- Api/Router/PostRouter.js
+++ Api/Router/PostRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getPost, getPosts, createPost, updatePost, deletePost } = require("../Controller/PostController");
+const { getPost, getPosts, createPost, updatePost, deletePost, getProfilePost } = require("../Controller/PostController");
 const verifyToken = require("../Middleware/verifyToken")
 const postRouter = express.Router();
 postRouter.get("/", getPosts);
 postRouter.get("/:id", getPost);
+postRouter.put("/profile/:id",verifyToken, getProfilePost);
+
 postRouter.put("/", verifyToken, createPost);
 postRouter.patch("/;id", verifyToken, updatePost);
 postRouter.delete("/:id", verifyToken, deletePost);
